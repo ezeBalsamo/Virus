3ac478d584ea06c5c281baaedc5379294b0cdf73
ezeBalsamo <alexander.ezequiel.balsamo@gmail.com>
2024-07-21 13:43:58 -0300

    :white_check_mark:(circular_iterator): add test: `one item`

--- tests/utils/test_circular_iterator.py
+++ tests/utils/test_circular_iterator.py
@@ -2,7 +2,17 @@ import pytest
 
 from virus.utils import CircularIterator
 
+pepe_sand = "Pepe Sand"
+
 
 def test_no_items() -> None:
     with pytest.raises(StopIteration):
         CircularIterator.from_items([])
+
+
+def test_one_item() -> None:
+    circular_iterator = CircularIterator.from_items([pepe_sand])
+
+    assert circular_iterator.current == pepe_sand
+    assert next(circular_iterator) == pepe_sand
+    assert circular_iterator.current == pepe_sand
