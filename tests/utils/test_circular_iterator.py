626f262352d0f1efbccee072eece06e8b2268e76
ezeBalsamo <alexander.ezequiel.balsamo@gmail.com>
2024-07-21 13:44:06 -0300

    :white_check_mark:(circular_iterator): add test: `many items`

--- tests/utils/test_circular_iterator.py
+++ tests/utils/test_circular_iterator.py
@@ -3,6 +3,8 @@ import pytest
 from virus.utils import CircularIterator
 
 pepe_sand = "Pepe Sand"
+lautaro_acosta = "Lautaro Acosta"
+diego_valeri = "Diego Valeri"
 
 
 def test_no_items() -> None:
@@ -14,5 +16,21 @@ def test_one_item() -> None:
     circular_iterator = CircularIterator.from_items([pepe_sand])
 
     assert circular_iterator.current == pepe_sand
+    assert next(circular_iterator) == pepe_sand
+    assert circular_iterator.current == pepe_sand
+
+
+def test_many_items() -> None:
+    items = [pepe_sand, lautaro_acosta, diego_valeri]
+    circular_iterator = CircularIterator.from_items(items)
+
+    assert circular_iterator.current == pepe_sand
+
+    assert next(circular_iterator) == lautaro_acosta
+    assert circular_iterator.current == lautaro_acosta
+
+    assert next(circular_iterator) == diego_valeri
+    assert circular_iterator.current == diego_valeri
+
     assert next(circular_iterator) == pepe_sand
     assert circular_iterator.current == pepe_sand
